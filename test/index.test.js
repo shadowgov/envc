80700e0537cd8bc9c842d0951735544c6f76c2ee
Veselin Todorov <veselin@veselin.bg>
2014-09-15 09:25:41 +0300

    Use the new parser

--- test/index.test.js
+++ test/index.test.js
@@ -11,12 +11,6 @@ test('custom location', function() {
   assert(env.ENVC_ONE);
 });
 
-test('do not write env variables that already exist', function() {
-  var path = process.env.PATH;
-  var env = envc('test/fixtures/path');
-  assert.equal(env.PATH, path);
-});
-
 test('prefer files with .env.{NODE_ENV} over .env', function() {
   var env = envc('test/fixtures/ignored');
   assert.equal(env.ENVC_SOURCE, 'test');
