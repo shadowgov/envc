89042cdd6457511058c731dcfd0ce03a774de16d
Wil Moore III <wil.moore@wilmoore.com>
2014-11-10 00:50:43 -0700

    return only parsed keys/values

--- test/index.test.js
+++ test/index.test.js
@@ -2,18 +2,23 @@ var assert = require('assert');
 var envc = require('../');
 
 test('default location', function() {
-  var env = envc();
-  assert(env.ENVC);
+  envc();
+  assert(process.env.ENVC);
+});
+
+test('updated keys', function() {
+  var keys = Object.keys(envc());
+  assert.deepEqual(keys, ['ENVC']);
 });
 
 test('custom location', function() {
-  var env = envc({ path: 'test/fixtures', name: 'envc-one' });
-  assert(env.ENVC_ONE);
+  envc({ path: 'test/fixtures', name: 'envc-one' });
+  assert(process.env.ENVC_ONE);
 });
 
 test('prefer files with .env.{NODE_ENV} over .env', function() {
-  var env = envc({ path: 'test/fixtures', name: 'ignored' });
-  assert.equal(env.ENVC_SOURCE, 'test');
+  envc({ path: 'test/fixtures', name: 'ignored' });
+  assert.equal(process.env.ENVC_SOURCE, 'test');
 });
 
 test('do not throw when the file cannot be found', function() {
@@ -23,7 +28,7 @@ test('do not throw when the file cannot be found', function() {
 });
 
 test('inheritance', function() {
-  var env = envc({ path: 'test/fixtures', name: 'inheritance' });
-  assert.equal(env.A, 1);
-  assert.equal(env.B, 3);
+  envc({ path: 'test/fixtures', name: 'inheritance' });
+  assert.equal(process.env.A, 1);
+  assert.equal(process.env.B, 3);
 });
